b0eb18691c686af9820d23ebfd57ab795e46a76b
Lasse Fisker Olesen <fiskero94@gmail.com>
2019-12-13 06:27:05 +0100

    stuff

---
+++ OrderManagement/ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductContext _productContext;
+    }
+}
