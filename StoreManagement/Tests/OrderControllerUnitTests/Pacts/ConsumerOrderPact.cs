54cdee0c69dbd16780152fe131f3180c450c4527
Mathias Nielsen <MathiasWael@hotmail.dk>
2019-12-15 08:47:56 +0100

    d

---
+++ StoreManagement/Tests/OrderControllerUnitTests/Pacts/ConsumerOrderPact.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using PactNet;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderControllerUnitTests.Pacts
+{
+    public class ConsumerOrderPact
+    {
+        public IPactBuilder PactBuilder { get; private set; }
+        public IMockProviderService MockProviderService { get; private set; }
+
+        public int MockServerPort { get { return 9222; } }
+        public string MockProviderServiceBaseUri { get { return String.Format("http://localhost:{0}", MockServerPort); } }
+
+        public ConsumerOrderPact()
+        {
+            PactBuilder = new PactBuilder();
+
+            PactBuilder
+              .ServiceConsumer("Consumer")
+              .HasPactWith("Something API");
+
+            MockProviderService = PactBuilder.MockService(MockServerPort);
+        }
+
+        public void Dispose()
+        {
+            PactBuilder.Build();
+        }
+    }
+}
---
+++ StoreManagement/Tests/OrderControllerUnitTests/Pacts/ConsumerOrderTests.cs
@@ -0,0 +1,35 @@
+﻿using PactNet.Mocks.MockHttpService;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using PactNet.Mocks.MockHttpService.Models;
+
+namespace OrderControllerUnitTests.Pacts
+{
+    public class ConsumerOrderTests : IClassFixture<ConsumerOrderPact>
+    {
+        private IMockProviderService mockProviderService;
+        private string mockProviderServiceBaseUri;
+
+        public ConsumerOrderTests(ConsumerOrderPact data)
+        {
+            mockProviderService = data.MockProviderService;
+            mockProviderService.ClearInteractions();
+            mockProviderServiceBaseUri = data.MockProviderServiceBaseUri;
+        }
+
+        [Fact]
+        public void ProductCreated_Ok()
+        {
+            mockProviderService
+                .Given("Product was created")
+                .UponReceiving("Post request for product")
+                .With(new ProviderServiceRequest
+                {
+                    Method = HttpVerb.Post,
+                    Path = ""
+                });
+        }
+    }
+}
